310476e0b3c9dcda6b0e634f4a28b58c6d7239cd
Graham Barber <grahammbarber@gmail.com>
2015-08-19 15:06:39 -0700

    Added Deepstream to server.js

--- platforms/web/config.js
+++ platforms/web/config.js
@@ -1,5 +1,9 @@
 module.exports = {
 	express: {
 		port: 3000
+	},
+	deepstream: {
+		host: "localhost",
+		port: "6020"
 	}
 }
--- platforms/web/server.js
+++ platforms/web/server.js
@@ -2,7 +2,9 @@ var express = require("express"),
 	app = express(),
 	bodyParser = require("body-parser"),
 	morgan = require("morgan"),
-	config = require(__dirname + "/config.js");
+	config = require(__dirname + "/config.js"),
+	DeepstreamServer = require("deepstream.io"),
+	server = new DeepstreamServer();
 
 app.use(express.static(__dirname + "/public"));
 app.use(morgan("dev"));
@@ -10,4 +12,9 @@ app.use(bodyParser.urlencoded({"extended": true}));
 app.use(bodyParser.json());
 
 app.listen(config.express.port);
-console.log("Dial " + config.express.port + " for more details");
+console.log("Dial " + config.express.port + " for the website.");
+
+server.set("host", config.deepstream.host);
+server.set("port", config.deepstream.port);
+server.start();
+console.log("Spin up http://" + config.deepstream.host + config.deepstream.port + " for a server connection.");
